2ca4c34306e51ca490bb3d395d4ea9cc8a86abbd
unknown <waelmanai96@gmail.com>
2023-11-26 19:28:09 +0100

    Creating Modal Provider and Create Server Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
-import { ThemeProvider } from '@/components/providers/theeme-provider'
+import { ThemeProvider } from "@/components/providers/theme-provider"
+import { ModalProvider } from "@/components/providers/modal-provider"
 import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
@@ -30,6 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="chatter-hub-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
