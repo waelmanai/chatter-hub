85282db7fb6a80c37609c87431289251d9cd7451
unknown <waelmanai96@gmail.com>
2024-02-28 23:00:39 +0100

    Channel creation

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
@@ -24,6 +25,7 @@ export const ModalProvider = () => {
             <InviteModal />
             <EditServerModal />
             <MembersModal />
+            <CreateChannelModal />
         </>
     )
 }
