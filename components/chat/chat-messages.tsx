a970a3ee22d19329774dfa0cb18bd1aa6cd1c00a
unknown <waelmanai96@gmail.com>
2024-03-13 17:17:03 +0100

    Chat Messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,9 +1,18 @@
 "use client";
 
-import { Member } from "@prisma/client";
+import { Member, Message, Profile } from "@prisma/client";
 
 import { ChatWelcome } from "./chat-welcome";
+import { useChatQuery } from "@/hooks/use-chat-query";
+import { Loader2, ServerCrash } from "lucide-react";
+import { Fragment } from "react";
 
+
+type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+}
 
 interface ChatMessagesProps {
     name: string;
@@ -28,6 +37,46 @@ export const ChatMessages = ({
     paramValue,
     type
 }: ChatMessagesProps) => {
+
+    const queryKey = `chat:${chatId}`;
+
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        status,
+    } = useChatQuery({
+        queryKey,
+        apiUrl,
+        paramKey,
+        paramValue
+    });
+
+    if (status === "error") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <ServerCrash className="w-7 h-7 text-zinc-500 my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Something went wrong!
+                </p>
+            </div>
+        )
+    }
+
+    if (status === "loading") {
+        return (
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <Loader2 className="w-7 h-7 animate-spin text-zinc-500 my-4" />
+                <p className="text-xs text-zinc-500 dark:text-zinc-400">
+                    Loading messages...
+                </p>
+            </div>
+        )
+    }
+
+
+
     return (
         <div className="flex-1 flex flex-col py-4 overflow-y-auto">
             <div className="flex-1" />
@@ -35,6 +84,18 @@ export const ChatMessages = ({
                 type={type}
                 name={name}
             />
+            1
+            <div className="flex flex-col-reverse mt-auto">
+                {data?.pages?.map((group, i) => (
+                    <Fragment key={i}>
+                        {group.items.map((message: MessageWithMemberWithProfile) => (
+                            <div key={message.id} className="">
+                                {message.content}
+                            </div>
+                        ))}
+                    </Fragment>
+                ))}
+            </div>
         </div>
     )
 }
